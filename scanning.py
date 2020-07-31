8116dadf6bd4ce8651198511bcb00f8e51ffcdae
Mozgawa <mateusz.mozgawa@gmail.com>
2020-07-31 19:32:52 +0200

    style(scanning): some small changes

--- scanning.py
+++ scanning.py
@@ -2,11 +2,10 @@ from PIL import Image, ImageEnhance
 
 
 def scan(filename):
-    im = Image.open("static/uploads/{}".format(filename))
-
-    # image brightness enhancer
-    enhancer = ImageEnhance.Brightness(im)
     factor = 2
+    angle = -90
+    im = Image.open("static/uploads/{}".format(filename))
+    enhancer = ImageEnhance.Brightness(im)
     im_output = enhancer.enhance(factor)
-    im_output = im_output.rotate(-90, expand=True)
+    im_output = im_output.rotate(angle, expand=True)
     im_output.save('static/uploads/brightened-image.png')
